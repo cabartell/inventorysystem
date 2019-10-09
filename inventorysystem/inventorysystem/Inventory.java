95fde1d279fda0312d84107ed04d79649e5bfb17
Cammie <cami834j@gmail.com>
2019-10-09 18:06:39 +0200

    added Inventory Class

--- inventorysystem/inventorysystem/Inventory.java
+++ inventorysystem/inventorysystem/Inventory.java
@@ -1,4 +1,32 @@
 package inventorysystem;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    private ArrayList<Item> items = new ArrayList<>();
+
+    public void additem(Item item){
+        items.add(item);
+    }
+
+    public void removeItem(Item item){
+        items.remove(item);
+    }
+
+    public double getInventoryValue(){
+        double inventoryValue = 0;
+        for (int i = 0 ; i < items.size() ; i++){
+            double x = items.get(i).getPrice();
+            inventoryValue = inventoryValue + x;
+        }
+        return inventoryValue;
+    }
+
+    public void printInventory(){
+        for(Item i : items)
+        {
+            System.out.println(i);
+        }
+    }
+
 }
